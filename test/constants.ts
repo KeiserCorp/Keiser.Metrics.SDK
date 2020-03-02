e16d10afed7fec2841cb7529c5f21d36dc80e59d
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2020-03-02 15:23:19 -0800

    Adds node support and tests (#3)

---
+++ test/constants.ts
@@ -0,0 +1,4 @@
+export const DevRestEndpoint = 'http://localhost:8000/api'
+export const DevSocketEndpoint = 'ws://localhost:8000/ws'
+export const IsBrowser = typeof window !== 'undefined'
+export const IsNode = !IsBrowser
---
+++ test/index.ts
@@ -0,0 +1,2 @@
+import './core.spec'
+import './connection.spec'
