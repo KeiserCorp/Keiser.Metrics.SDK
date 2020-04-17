399319eaac444736004de52e012dafc3373737ca
Zachary Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2020-04-17 14:38:16 -0700

    Removes DTS bundler

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
@@ -13,39 +12,6 @@ const package = Object.assign(require('../package.json'), {
   devDependencies: {},
   scripts: {}
 })
-
-function DtsBundlePlugin(options) { this.options = options }
-DtsBundlePlugin.prototype.apply = function (compiler) {
-  compiler.hooks.afterEmit.tap('DtsBundlePlugin', () => {
-    require('dts-bundle').bundle(this.options)
-    replace(this.options.out, [
-      {
-        search: /^\/\/.*\r?\n/gm,
-        replace: ''
-      },
-      {
-        search: /^[\t ]*\r?\n/gm,
-        replace: ''
-      },
-      {
-        search: /^\}(?:\r?\n|\r)(?!$)/gm,
-        replace: '}\n\n'
-      }
-    ])
-  })
-}
-
-function replace(file, rules) {
-  const src = path.resolve(file)
-  let template = fs.readFileSync(src, 'utf8')
-
-  template = rules.reduce(
-    (template, rule) => template.replace(rule.search, rule.replace),
-    template
-  )
-
-  fs.writeFileSync(src, template)
-}
 
 const baseConfig = {
   mode: 'production',
