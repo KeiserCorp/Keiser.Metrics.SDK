b665beafebc7bc2fc83e37fdbbcd94f32d6d9d1e
Ismael Huerta <imartinez2028@gmail.com>
2021-01-20 07:25:50 -0800

    Adds hidden sessionPlanSequenceInstance (#55)
    * Adds hidden sessionPlanSequenceInstance
    
    Co-authored-by: Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>

--- src/models/session.ts
+++ src/models/session.ts
@@ -115,6 +115,15 @@ export class StaticSession {
   get weightMeasurement () {
     return this._sessionData.weightMeasurement
   }
+
+  /**
+   * @deprecated This endpoint should does not expose a proper model and should will
+   * be removed in the next major version release.
+   * @hidden
+   */
+  get sessionPlanSequenceInstance () {
+    return this._sessionData.sessionPlanSequenceInstance
+  }
 }
 
 export class Session extends Model {
@@ -163,6 +172,15 @@ export class Session extends Model {
 
   get endedAt () {
     return this._sessionData.endedAt !== null ? new Date(this._sessionData.endedAt) : null
+  }
+
+  /**
+   * @deprecated This endpoint should does not expose a proper model and should will
+   * be removed in the next major version release.
+   * @hidden
+   */
+  get sessionPlanSequenceInstance () {
+    return this._sessionData.sessionPlanSequenceInstance
   }
 
   eagerUser () {
