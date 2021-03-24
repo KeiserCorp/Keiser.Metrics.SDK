fc83b2942372f35d96665ea89e70129b82805626
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2021-03-23 21:14:15 -0700

    Adds static A500 machien state

--- src/models/a500MachineState.ts
+++ src/models/a500MachineState.ts
@@ -48,3 +48,27 @@ export class A500MachineState extends Model<StrengthMachineSessionHandler> {
     return this._a500MachineState.secondaryFocus
   }
 }
+
+export class StaticA500MachineState {
+  private readonly _a500MachineState: A500MachineStateData
+
+  constructor (a500MachineStateData: A500MachineStateData) {
+    this._a500MachineState = a500MachineStateData
+  }
+
+  get forceUnits () {
+    return this._a500MachineState.forceUnits
+  }
+
+  get primaryFocus () {
+    return this._a500MachineState.primaryFocus
+  }
+
+  get secondaryFocus () {
+    return this._a500MachineState.secondaryFocus
+  }
+
+  public toJSON () {
+    return this._a500MachineState
+  }
+}
